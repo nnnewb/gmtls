216ea80d02d83ffddd323dab31bb96df9fe5b1ab
左思 <weak_ptr@163.com>
2025-01-09 00:25:19 +0800

    feat: 实现握手协议族密码规格变更协议消息

---
+++ internal/handshake/change_cipher_spec.go
@@ -0,0 +1,25 @@
+package handshake
+
+// ChangeCipherSpecMessage 握手协议族中的密码规格变更协议。定义于 GM/T 0024-2014 第 6.4.1 节。
+type ChangeCipherSpecMessage struct {
+	Type ChangeCipherSpecType
+}
+
+// ChangeCipherSpecType 密码规格变更协议的 Type 枚举值，定义于 GM/T 0024-2014 第 6.4.1 节。
+type ChangeCipherSpecType uint8
+
+const (
+	ChangeCipherSpecTypeChangeCipherSpec ChangeCipherSpecType = 1
+	ChangeCipherSpecTypeMaximum          ChangeCipherSpecType = 255
+)
+
+var _ = ChangeCipherSpecTypeMaximum
+
+func (c ChangeCipherSpecType) String() string {
+	switch c {
+	case ChangeCipherSpecTypeChangeCipherSpec:
+		return "change_cipher_spec"
+	default:
+		return "unknown"
+	}
+}
